5c56cfbace00285bfc499d7d92dd9c23486d8363
victoria-peinado <victoria-peinado@u.com>
2019-09-12 00:19:36 -0300

    ....

--- tests/MedioTest.php
+++ tests/MedioTest.php
@@ -38,7 +38,7 @@ class MedioTest extends TestCase
         $tiempo->avanzar(300);
         $this->assertEquals($medio->restarSaldo("153"), false);//no puedo pagar el pasaje
         $this->assertTrue($medio->recargar(962.59)); //recarga 1184.14
-        $this->assertEquals($medio->obtenerSaldo(), 1174.54);// no es 1159.77 porque no se resta el plus al recargar
+        $this->assertEquals($medio->obtenerSaldo(), 1189.34);// no es 1159.77 porque no se resta el plus al recargar
         $this->assertEquals($medio->restarSaldo("153"), true);
         $this->assertEquals($medio->restarSaldo("153"), false);
         $this->assertEquals($medio->restarSaldo("153"), false);
