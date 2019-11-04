25479b3bd0d9208ca2c7bd7c1c69a370ae326183
victoria-peinado <victoria-peinado@u.com>
2019-11-04 18:11:27 -0300

    estilo

--- src/Pagable.php
+++ src/Pagable.php
@@ -13,7 +13,7 @@ class Pagable extends Recargable
     /**
      * Funcion para pagar plus en caso de deberlos.
      */
-     protected function pagarPlus()
+    protected function pagarPlus()
      {
          if ($this->plus == 2) { //Si debe 2 plus
              if ($this->saldo >= ($this->ValorBoleto * 2)) { //Y si le alcanza el saldo para pagarlos
@@ -65,5 +65,46 @@ class Pagable extends Recargable
          return false; // No fue posible pagar
      }
  
-      //todo lo antarior iria en la clase pagar
+     /**
+     * Devuelve el ultimo valor pagado.
+     *
+     * @return float
+     */
+    public function valorPagado()
+     {
+        return $this->UltimoValorPagado; // Devuelve el ultimo valor que se pago
+     }
+   
+    /**
+    * Devuelve la ultima hora en la que se uso la tarjeta.
+    *
+    * @return int
+    */
+    public function ultimaHoraUsada()
+     {
+         return $this->UltimaHora; // Devuelve la ultima hora a la que se pago
+     }
+
+    /**
+    * Devuelve si se utilizo un viaje plus.
+    *
+    * @return int
+    */
+    public function usoPlus()
+     {
+         return $this->plus; // Devuelve si se utilizo un viaje plus
+     }
+
+    /**
+     * Setea a 0 el "pago plus". Esta funcion se ejecutara cuando se emite el boleto.
+     *
+     * @return int
+     *   La cantidad de plus que pago en la ultiima recarga.
+     */
+    public function obtenerPagoPlus()
+     {
+         $pagoplusaux = $this->pagoplus; // Se almacena en un auxiliar
+         $this->pagoplus = 0; // se Reinicia
+         return $pagoplusaux; // Se devuelve el auxiliar
+     }
 }
--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -25,18 +25,7 @@ class Tarjeta extends Trasbordable  implements TarjetaInterface
     
     
 
-    /**
-     * Setea a 0 el "pago plus". Esta funcion se ejecutara cuando se emite el boleto.
-     *
-     * @return int
-     *   La cantidad de plus que pago en la ultiima recarga.
-     */
-    public function obtenerPagoPlus()
-    {
-        $pagoplusaux = $this->pagoplus; // Se almacena en un auxiliar
-        $this->pagoplus = 0; // se Reinicia
-        return $pagoplusaux; // Se devuelve el auxiliar
-    }
+
 
     /**
      * Devuelve el valor completo del boleto.
@@ -58,33 +47,6 @@ class Tarjeta extends Trasbordable  implements TarjetaInterface
         return $this->id; //Devuelve el id de la tarjeta
     }
 
-    /**
-     * Devuelve el ultimo valor pagado.
-     *
-     * @return float
-     */
-    public function valorPagado()
-    {
-        return $this->UltimoValorPagado; // Devuelve el ultimo valor que se pago
-    }
 
-    /**
-     * Devuelve la ultima hora en la que se uso la tarjeta.
-     *
-     * @return int
-     */
-    public function ultimaHoraUsada()
-    {
-        return $this->UltimaHora; // Devuelve la ultima hora a la que se pago
-    }
-
-    /**
-     * Devuelve si se utilizo un viaje plus.
-     *
-     * @return int
-     */
-    public function usoPlus()
-    {
-        return $this->plus; // Devuelve si se utilizo un viaje plus
-    }
+   
 }
