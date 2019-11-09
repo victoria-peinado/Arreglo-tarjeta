cd2cbc056f60da20c9b11c11f38ae8c8ac5f0b9d
victoria-peinado <victoria-peinado@u.com>
2019-11-09 15:59:20 -0300

    ok, comentarios feos

--- src/Medio.php
+++ src/Medio.php
@@ -19,28 +19,7 @@ class Medio extends Tarjeta
      *   Si se pudo realizar el pago. 
      * NO ES CORRECTO QUE EXTIENDA TARJETA SI DESPUES VA A SOBRE ESCRIBIR EL METODO
      */
-    """
-    public function restarSaldo($linea)
-    {
-		$this->pagarPlus();//ESTE SERIA EL LUGAR CORRECTO PARA RESTAR LOS PLUS
-        if (($this->tiempo->time() - $this->UltimaHora) < 299) {return false;} //Limitacion de 5 minutos
-        $ValorARestar = $this->calculaValor($linea); //Llama a la funcion que calcula el valor del boleto a pagar
-        if ($this->saldo >= $ValorARestar) { //Se fija si le alcanza el saldo
-            $this->saldo -= $ValorARestar; //En caso de alcanzar lo resta
-            $this->UltimoValorPagado = $ValorARestar; //Guarda el valor del ultimo pago que se realizo
-            $this->UltimoColectivo = $linea;
-            $this->UltimaHora = $this->tiempo->time(); //Guarda la hora de este pago
-            return true; //se completa el pago
-        }
-        if ($this->plus < 2) { //En caso de no alcanzarle el saldo, se fija si dispone de plus
-            $this->plus++; //le saca un plus
-            $this->UltimaHora = $this->tiempo->time(); //Guarda la hora de utilizacion del plus
-            $this->UltimoColectivo = $linea;
-            $this->UltimoValorPagado = 0.0; //Indica que se pago 0.0
-            return true; //Completa el pago
-        }
-        return false; //No Se pudo pagar
-    }"""
+
 
     /**
      * Devuelve el valor del boleto a pagar, pero antes se fija si puede hacer un trasbordo utilizando al otra funcion.
