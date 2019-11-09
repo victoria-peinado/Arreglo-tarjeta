a990ccc9f65df9092bd0b5c7aae133fbacb0c29e
victoria-peinado <victoria-peinado@u.com>
2019-11-09 14:40:10 -0300

    ya no se

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -59,7 +59,9 @@ class Colectivo implements ColectivoInterface
      */
     public function pagarCon(TarjetaInterface $tarjeta)
     {
-        if (!($this->pagable->PrestarSaldo($this->linea,$tarjeta))) { //Si la funcion para restar el saldo retorna false
+        echo $this->linea;
+        echo $tarjeta->monto;
+        if (!($this->pagable->PrestarSaldo(($this->linea),$tarjeta))) { //Si la funcion para restar el saldo retorna false
             return false; //Falla el pago
         }
         return (new Boleto($this, $tarjeta)); //Crea un boleto con la informacion
