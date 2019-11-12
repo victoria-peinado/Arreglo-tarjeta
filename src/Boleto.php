0c9e58428f6408a5569a3071dcb9faf860df5424
victoria-peinado <victoria-peinado@u.com>
2019-11-12 18:39:40 -0300

    si no cambio la interface no va funcionar nunca

--- src/Boleto.php
+++ src/Boleto.php
@@ -4,32 +4,7 @@ namespace TrabajoTarjeta;
 
 class Boleto implements BoletoInterface
 {
-
-    protected $valor; //Valor pagado
-
-    protected $colectivo; //Colectivo en el que se pago
-
-    protected $tarjeta; //Tarjeta con la que se pago
-
-    protected $cantplus; //Si se pagaron plus en la ultima recarga
-
-    protected $hora; //hora de pago
-
-    protected $idtarjeta; //id de la tarjeta
-
-    protected $boletoCompleto; //Valor de un boleto completo de colectivo para el calculo del plus
-
-    protected $linea; //Linea del colectivo
-
     protected $saldo; //Saldo restante
-
-    protected $descripcion; //Descripcion del boleto plus
-
-    protected $Tipo; //Tipo de tarjeta que se utilizo
-
-    protected $usoPlus; //Si se utilizaron plus
-
-    protected $PagoPlus;
 
     public function __construct($tarjeta)
     {
