ac04e5acac54c727b4a3c88c38c9c87777835546
AlejoChol <AlejoChol@users.noreply.github.com>
2019-11-12 16:01:48 -0300

    Arreglado MedioTest

--- tests/MedioTest.php
+++ tests/MedioTest.php
@@ -26,30 +26,45 @@ class MedioTest extends TestCase
         $tiempo = new TiempoFalso;
         $recargable = new Recargable();
         $medio = new Medio(0, $tiempo,$recargable);
-        $this->assertTrue($medio->recargar(20));
-        $this->assertEquals($medio->obtenerSaldo(), 20);
-        $this->assertEquals($medio->restarSaldo("153"), true);//valor pasaje 7.4
+		$saldoEsperado =0;
+        $this->assertTrue($medio->recargar(100));//Prueba si carga
+		$saldoEsperado=$saldoEsperado+100
+        $this->assertEquals($medio->obtenerSaldo(), $saldoEsperado);
+
+        $this->assertEquals($medio->restarSaldo("153"), true);//Medio Comun
+		$saldoEsperado=$saldoEsperado-($medio->ValorBoleto /2);
+        $this->assertEquals($medio->obtenerSaldo(), $saldoEsperado);
+
         $tiempo->avanzar(300);
-        $this->assertEquals($medio->obtenerSaldo(), 12.6);
+        $this->assertEquals($medio->restarSaldo("153"), true);//Comprueba que avanzar 5 minutos permite usar un medio
+		$saldoEsperado=$saldoEsperado-(($medio->ValorBoleto /2));
+        $this->assertEquals($medio->obtenerSaldo(), $saldoEsperado);
+		
+        $tiempo = new TiempoFalso;
+        $recargable = new Recargable();
+        $medio = new Medio(0, $tiempo,$recargable);
+		$saldoEsperado =0;
+
+        $tiempo->avanzar(300);
+        $this->assertEquals($medio->restarSaldo("153"), true);	//debe un medio
+        $tiempo->avanzar(300);
+        $this->assertEquals($medio->restarSaldo("153"), true);	//debe otro medio
+        $tiempo->avanzar(300);
+        $this->assertEquals($medio->restarSaldo("153"), false);	//no puedo pagar el pasaje
+
+        $this->assertTrue($medio->recargar(1119.90));			//recarga 1300.00
+		$saldoEsperado=$saldoEsperado+1300.00
+
+        $this->assertEquals($medio->obtenerSaldo(), $saldoEsperado);	// no es 1159.77 porque no se resta el plus al recargar
         $this->assertEquals($medio->restarSaldo("153"), true);
-        $this->assertEquals($medio->obtenerSaldo(), 5.2);
+        $this->assertEquals($medio->restarSaldo("153"), false);	//Comprueba que sin avanzar 5 minutos no se puede realizar otro pago de boleto
         $tiempo->avanzar(300);
-        $this->assertEquals($medio->restarSaldo("153"), true);//debe un medio
-        $tiempo->avanzar(300);
-        $this->assertEquals($medio->restarSaldo("153"), true);//debe otro medio
-        $tiempo->avanzar(300);
-        $this->assertEquals($medio->restarSaldo("153"), false);//no puedo pagar el pasaje
-        $this->assertTrue($medio->recargar(962.59)); //recarga 1184.17
-        $this->assertEquals($medio->obtenerSaldo(), 1189.37);// no es 1159.77 porque no se resta el plus al recargar
-        $this->assertEquals($medio->restarSaldo("153"), true);
-        $this->assertEquals($medio->restarSaldo("153"), false);
-        $this->assertEquals($medio->restarSaldo("153"), false);
-        $this->assertEquals($medio->restarSaldo("153"), false);
-        $tiempo->avanzar(300);
+
         for (($i = 0); $i < 155; ++$i) {
             $this->assertEquals($medio->restarSaldo("153"), true);
             $tiempo->avanzar(300);
         }
+
         $this->assertEquals($medio->restarSaldo("153"), true);
         $this->assertEquals($medio->restarSaldo("153"), false);
         $tiempo->avanzar(300);
@@ -65,10 +80,16 @@ class MedioTest extends TestCase
         $tiempo = new TiempoFalso;
         $recargable = new Recargable();
         $medio = new Medio(0, $tiempo,$recargable);
-        $this->assertTrue($medio->recargar(962.59));
+		$saldoEsperado =0;
+
+        $this->assertTrue($medio->recargar(1119.90));	//Carga 1300.00
         $this->assertEquals($medio->restarSaldo("153"), true);
+		$saldoEsperado =$saldoEsperado+1300.00;
+
         $tiempo->avanzar(300);
         $this->assertEquals($medio->restarSaldo("153"), true);
+		$saldoEsperado =$saldoEsperado-($medio->ValorBoleto/2);
+
         $tiempo->avanzar(50);
         $this->assertEquals($medio->restarSaldo("153"), false);
         $tiempo->avanzar(50);
@@ -79,8 +100,11 @@ class MedioTest extends TestCase
         $this->assertEquals($medio->restarSaldo("153"), false);
         $tiempo->avanzar(50);
         $this->assertEquals($medio->restarSaldo("153"), false);
+
         $tiempo->avanzar(50);
         $this->assertEquals($medio->restarSaldo("153"), true);
+		$saldoEsperado=$saldoEsperado-($medio->ValorBoleto/2);
+
         $tiempo->avanzar(265);
         $this->assertEquals($medio->restarSaldo("153"), false);
         $tiempo->avanzar(584);
@@ -101,19 +125,24 @@ class MedioTest extends TestCase
         $tarjeta->recargar(100);
         $colectivo1 = new Colectivo(122, "Semtur", 37);
         $colectivo2 = new Colectivo(134, "RosarioBus", 52);
+		$saldoEsperado =200;
 
         /*
         Pruebo pagar un trasbordo un dia feriado con 90 minutos de espera y el texto del boleto
          */
         $boleto = $colectivo1->pagarCon($tarjeta);
+		$saldoEsperado=$saldoEsperado-($medio->ValorBoleto/2);
         $this->assertEquals(date('N', $tiempo->time()), '4');
         $this->assertEquals(date('G', $tiempo->time()), '8');
         $this->assertEquals(date('d-m', $tiempo->time()), "01-01");
         $this->assertEquals($boleto->obtenerFecha(), "01/01/1970 08:00:00");
-        $this->assertEquals($tarjeta->obtenerSaldo(), 192.6);
+        $this->assertEquals($tarjeta->obtenerSaldo(), $saldoEsperado);
+
         $tiempo->avanzar(4200);
+		$saldoEsperado=$saldoEsperado-(($medio->ValorBoleto/2)*0.33);
+		$stringEsperado="Trasbordo Medio " . $$saldoEsperado;
         $boleto2 = $colectivo2->pagarCon($tarjeta);
-        $this->assertEquals($boleto2->obtenerDescripcion(), "Trasbordo Medio 2.442");
-        $this->assertEquals($tarjeta->obtenerSaldo(), 190.158);
+        $this->assertEquals($boleto2->obtenerDescripcion(), $stringEsperado);
+        $this->assertEquals($tarjeta->obtenerSaldo(), $saldoEsperado);
     }
 }
