bb326b841943f91ce40c6f752349879c9f6a90cd
AlejoChol <AlejoChol@users.noreply.github.com>
2019-11-12 23:28:38 -0300

    probando

--- src/Recargable.php
+++ src/Recargable.php
@@ -3,7 +3,7 @@ namespace TrabajoTarjeta;
 
 class Recargable 
 {
-	$constantes = new VariablesConstantes;
+	protected $constantes = new VariablesConstantes;
     //protected $saldo = 0;
     
        /**
